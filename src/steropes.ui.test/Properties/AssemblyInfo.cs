ec88cd43ff0aeca7d61240e4ba090070bdfd8f1c
Thomas Morgner <tmorgner@tmorgner.com>
2018-03-25 16:24:14 +0100

    Updating version info for release branch 2.0.1

--- src/steropes.ui.test/Properties/AssemblyInfo.cs
+++ src/steropes.ui.test/Properties/AssemblyInfo.cs
@@ -29,4 +29,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyFileVersion("2.0.1.0")]
-[assembly: AssemblyInformationalVersion("2.0.1-beta.1+0.Branch.release-2.0.1.Sha.78711cdadb0d725610cdcae15822c77bf7396a0b")]
+[assembly: AssemblyInformationalVersion("2.0.1+6.Branch.master.Sha.f49419c8492b2ed7479ff65287a60a1b99e58824")]
